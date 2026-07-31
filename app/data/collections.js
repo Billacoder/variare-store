3ba3a1a9a96828898d7c928e623c135a153619f2
billacoder <billamuti3@gmail.com>
2026-07-31 19:42:40 +0530

    fixing errors 7

--- app/data/collections.js
+++ app/data/collections.js
@@ -2,40 +2,40 @@ export const collections = [
 	{
 		title: "Wearables",
 		slug: "wearables",
-		image: "/chrochet products/tops/43065740182406141.jpg",
+		image: "/collections/wearables/photo 8.jpg",
 		rowSpan: "row-span-7",
 	},
 	{
 		title: "Crochet Plush",
 		slug: "crochet-plush",
-		image: "/chrochet products/stuffed animals/I Made These Crochet Pieces.jpg",
+		image: "/collections/wearables/photo 8.jpg",
 		rowSpan: "row-span-5",
 	},
 	{
 		title: "Little Knits",
 		slug: "little-knits",
 		image:
-			"/chrochet products/baby/CROCHET PATTERN Baby Flower Dress, Sunflower Granny Square, Baby Sundress, 0-24 Months, PDF Download - Etsy.jpg",
+			"/collections/wearables/photo 8.jpg",
 		rowSpan: "row-span-6",
 	},
 	{
 		title: "Bags",
 		slug: "bags",
 		image:
-			"/chrochet products/bags/Handmade Woven Crochet Bag_ Two-Tone T-Shirt Yarn Purse.jpg",
+			"/collections/wearables/photo 8.jpg",
 		rowSpan: "row-span-8",
 	},
 	{
 		title: "Beanies",
 		slug: "beanies",
-		image: "/chrochet products/beanie/Animal-Themed Crochet Hats.jpg",
+		image: "/collections/wearables/photo 8.jpg",
 		rowSpan: "row-span-7",
 	},
 	{
 		title: "Blankets",
 		slug: "blankets",
 		image:
-			"/chrochet products/blanket/Easy Baby Blanket Crochet Pattern FREE - 2 color crochet blanket.jpg",
+			"/collections/wearables/photo 8.jpg",
 		rowSpan: "row-span-6",
 	},
 ];
